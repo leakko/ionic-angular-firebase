5b2e2f14e5c917981ed67ed0e65acf95ca858404
Marcos Rodriguez Salvo <marcossalvo95@gmail.com>
2022-06-25 16:27:31 +0200

    Integration completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideRemoteConfig(() => getRemoteConfig()), provideStorage(() => getStorage())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'functions-tutorial-f00b5',
+    appId: '1:274110823672:web:6d9f4544e60ef4580c8f0d',
+    storageBucket: 'functions-tutorial-f00b5.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyD9jRjuZnX55p63RfB_dxGK5l6xvWdXqMA',
+    authDomain: 'functions-tutorial-f00b5.firebaseapp.com',
+    messagingSenderId: '274110823672',
+  },
   production: false
 };
 
